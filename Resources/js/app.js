31d6e3b6f2389494e4f4a49d0fe257fa57b17df1
Malebestia <malebestia@yahoo.it>
2022-05-09 19:19:37 +0200

    first

---
+++ Resources/js/app.js
@@ -0,0 +1,33 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+require('./myvue3');
+
+//import videojs from 'video.js'
+/*
+npm
+"vue": "^2.6.14",
+        "vue-loader": "^15.9.7",
+        "vue-template-compiler": "^2.6.14",
+*/
+
+require('./btnDeleteX2.js');
+require('./flatpickr.js');
+
+//$(document).ready(function() {
+$(function() {
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue'
+    });
+
+    $('.js-datepicker').datepicker({
+        autoclose: true,
+        todayHighlight: true
+    })
+});
---
+++ View/Components/BaseLayout.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Themes\AdminLTE\View\Components;
+
+use Illuminate\View\Component;
+
+class BaseLayout extends Component {
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render():\Illuminate\Contracts\Support\Renderable {
+        return view('layouts.base');
+    }
+}
