0e49c35a7639fb91953a3c48ca49f171e57992dc
marco xot <marco.sottana@gmail.com>
2022-06-21 19:59:15 +0200

    up

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -1,5 +1,25 @@
+{{--
 @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
 @endsection
+--}}
+<x-dashboard>
+    {{-- replace this by any tiles --}}
+    {{--
+    <livewire:time-weather-tile position="a1" />
+    <livewire:time-weather-tile position="b1" />
+    --}}
+    <livewire:dashboard.tile.dummy position="c1" />
+    <livewire:time-weather-tile position="d1" />
+
+    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}" position="a1:b2" />
+    
+    {{--
+    @livewire('chart-tile', [
+    'chartClass' => Modules\LU\Charts\DailyUsersChart::class, 
+    'chartFilters' => ['type' => 'customer'],
+    ])
+    --}}
+</x-dashboard>
 
