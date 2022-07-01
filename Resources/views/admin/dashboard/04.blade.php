9f565464a6f0dca50dfac6b7f23f08160d4d3bed
Malebestia <vair81@gmail.com>
2022-07-01 18:12:52 +0200

    .

---
+++ Resources/views/admin/dashboard/04.blade.php
@@ -0,0 +1,13 @@
+@extends('adm_theme::layouts.app')
+@section('content')
+    @php
+    //dddx($profile->getProfile()->customers);
+    $customer = $profile->getProfile()->customers->first();
+    $survey_pdf_first = $customer->surveyPdfs->first();
+    @endphp
+
+    @foreach ($survey_pdf_first->questionCharts as $question_chart)
+        <p>{{ $question_chart->question_txt }}</p>
+        <x-lime.chart type="chartjs" question_chart_id="{{ $question_chart->id }}"></x-lime.chart>
+    @endforeach
+@endsection
