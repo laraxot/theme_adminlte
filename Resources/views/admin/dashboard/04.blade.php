cee81209ec11bb66da63faad26774024aa6ce98c
Malebestia <vair81@gmail.com>
2022-07-02 18:13:57 +0200

    .

--- Resources/views/admin/dashboard/04.blade.php
+++ Resources/views/admin/dashboard/04.blade.php
@@ -4,14 +4,17 @@
     //dddx($profile->getProfile()->customers);
     $customer = $profile->getProfile()->customers->first();
     $survey_pdf_first = null;
+    $question_charts = null;
     if (!is_null($customer)) {
-        $survey_pdf_first = $customer->surveyPdfs->first();
+        $survey_pdf_first = $customer->surveyPdfs->last();
+        $question_charts = $survey_pdf_first->questionCharts;
     }
     @endphp
 
-
-    @foreach ($survey_pdf_first->questionCharts as $question_chart)
-        <p>{{ $question_chart->question_txt }}</p>
-        <x-lime.chart type="chartjs" id="{{ $question_chart->id }}"></x-lime.chart>
-    @endforeach
+    @if (!is_null($question_charts))
+        @foreach ($survey_pdf_first->questionCharts as $question_chart)
+            <p>{{ $question_chart->question_txt }}</p>
+            <x-lime.chart type="chartjs" id="{{ $question_chart->id }}"></x-lime.chart>
+        @endforeach
+    @endif
 @endsection
