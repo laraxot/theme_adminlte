5ca16023133389d8cfb174bf9b6d17216910ca6a
marco xot <marco.sottana@gmail.com>
2022-07-07 22:05:02 +0200

    .

--- Resources/views/admin/dashboard/01.blade.php
+++ Resources/views/admin/dashboard/01.blade.php
@@ -1,4 +1,7 @@
 @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
+    @foreach($profile->areas() as $area)
+        <x-dashboard.item.area :area="$area" ></x-dashboard.item.area>
+    @endforeach
 @endsection
