bd0a1cf04706045c0aa3b1f3583648077d51b4b9
marco76tv <marco.sottana@gmail.com>
2022-07-13 11:58:14 +0200

    .

--- Resources/views/admin/dashboard/01.blade.php
+++ Resources/views/admin/dashboard/01.blade.php
@@ -233,9 +233,11 @@ aaa
 @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
+    <div class="row">
     @foreach($profile->areas() as $area)
         <x-dashboard.item.area :area="$area" ></x-dashboard.item.area>
     @endforeach
+    </div>
 @endsection
 <<<<<<< HEAD
 <<<<<<< HEAD
