4ff622c15189905ba63d9a8f2ea2007768771485
marco76tv <marco.sottana@gmail.com>
2022-08-06 14:13:10 +0000

    .

---
+++ Resources/views/admin/dashboard/05.blade.php
@@ -0,0 +1,11 @@
+@php
+//$customer=\Modules\Quaeris\Models\Customer::first();
+//$profile->getProfile()->customers()->sync($customer);
+//dddx($profile->getProfile()->customers()->get());
+$first_customer = $profile->getProfile()->customers->first();
+//dddx($first_customer);
+$first_survey_pdf = $first_customer->surveyPdfs->first();
+$question_chart_first = $first_survey_pdf->questionCharts->first();
+@endphp
+<x-lime.chart-img questionChartId="4">
+    </x-lime.chart>
