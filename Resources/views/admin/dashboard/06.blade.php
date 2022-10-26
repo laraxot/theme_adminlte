7e04d1e9d8973b6b1c11bfd316f516558e48ed9a
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 17:00:10 +0000

    rebase

--- Resources/views/admin/dashboard/06.blade.php
+++ Resources/views/admin/dashboard/06.blade.php
@@ -1,5 +1,6 @@
 @extends('adm_theme::layouts.app')
 @section('content')
+<<<<<<< HEAD
 
     @php
     //dddx($profile->getProfile()->customers);
@@ -11,20 +12,39 @@
     //dddx($profile->getProfile()->customers);
     $customer = $profile->getProfile()->customers;
 
+=======
+    @php
+    //dddx($profile->getProfile()->customers);
+    /*
+    $customer_rnd=\Modules\Quaeris\Models\Customer::inRandomOrder()->first();
+    $profile->getProfile()->customers()->sync($customer_rnd);
+    */
+    //dddx($customer_rnd);
+    $customer = $profile->getProfile()->customers->first();
+    
+>>>>>>> e45345e (.)
     $survey_pdf_first = null;
     $question_charts = null;
     if (!is_null($customer)) {
         $survey_pdf_first = $customer->surveyPdfs->last();
         $question_charts = $survey_pdf_first->questionCharts;
     }
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> e45345e (.)
     @endphp
 
     @if (!is_null($question_charts))
         @foreach ($survey_pdf_first->questionCharts as $question_chart)
             <p>{{ $question_chart->question_txt }}</p>
+<<<<<<< HEAD
             <x-lime.chart-img.v2 questionChartId="{{ $question_chart->id }}">
             </x-lime.chart-img.v2>
+=======
+            <x-lime.chart-img.v2  questionChartId="{{ $question_chart->id }}"></x-lime.chart-img>
+>>>>>>> e45345e (.)
         @endforeach
     @endif
 @endsection
