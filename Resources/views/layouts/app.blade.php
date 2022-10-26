461884379efb6cf56285025c717de97f5cb6ac92
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 17:00:58 +0000

    rebase

--- Resources/views/layouts/app.blade.php
+++ Resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 @extends('adm_theme::layouts.plane')
 
-@inject('layoutHelper', 'Themes\AdminLTE\Helpers\LayoutHelper')
+@inject('layoutHelper', 'Themes\KlekooAdm\Helpers\LayoutHelper')
 
 @section('adminlte_css')
     @stack('css')
@@ -12,7 +12,9 @@
 
 @section('body')
 
-
+@php
+dddx(get_defined_vars())
+@endphp
 
     <div class="wrapper">
 <<<<<<< HEAD
