972b9f323ec19027495603db0dc45e61517e3503
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 16:59:40 +0000

    rebase

--- Resources/views/admin/dashboard/01.blade.php
+++ Resources/views/admin/dashboard/01.blade.php
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 @extends('adm_theme::layouts.app')
 @section('content')
     @php
@@ -47,8 +48,50 @@ aaa
 </x-dashboard>
 >>>>>>> 870e170 (.)
 =======
+=======
+>>>>>>> 49899e0 (.)
+@extends('adm_theme::layouts.app')
+@section('content')
+    DASHBOARD
+@endsection
+<<<<<<< HEAD
+>>>>>>> 7306100 (.)
+=======
+=======
+<<<<<<< HEAD
+{{-- @extends('adm_theme::layouts.app')
+@section('content')
+    DASHBOARD
+@endsection --}}
+
+
+
+<x-dashboard>
+    {{-- replace this by any tiles --}}
+    {{-- <livewire:time-weather-tile position="a1" />
+    <livewire:time-weather-tile position="b1" /> --}}
+
+    {{-- <livewire:dashboard.tile.dummy position="c1" /> --}}
+    <livewire:time-weather-tile position="a1:a2" />
+    <livewire:chart-tile chartClass="{{ Modules\LU\Charts\DailyUsersChart::class }}" position="b1:b2" />
+
+    <livewire:chart-tile chartClass="{{ Modules\Chart\Charts\ExampleBarChart::class }}" position="a3:a4" />
+    {{-- <livewire:dashboard.tile.question-chart position="a3:a4" qid="15" /> --}}
+
+
+    @livewire('chart-tile', [
+        'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class,
+        'chartFilters' => ['question_chart_id' => 5],
+        'position' => 'b3:b4',
+    ])
+
+
+</x-dashboard>
+=======
 @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
 @endsection
 >>>>>>> 7306100 (.)
+>>>>>>> fac8ac6 (.)
+>>>>>>> 49899e0 (.)
