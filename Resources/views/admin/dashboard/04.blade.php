8988d7194ed12bd438f3fb94732ed89b00f19b21
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 17:00:07 +0000

    rebase

--- Resources/views/admin/dashboard/04.blade.php
+++ Resources/views/admin/dashboard/04.blade.php
@@ -1,6 +1,7 @@
 @extends('adm_theme::layouts.app')
 @section('content')
     @php
+<<<<<<< HEAD
 
     //dddx($profile->getProfile()->customers);
     $customer = $profile->getProfile()->customers->first();
@@ -18,4 +19,15 @@
             <x-lime.chart type="chartjs" id="{{ $question_chart->id }}"></x-lime.chart>
         @endforeach
     @endif
+=======
+    //dddx($profile->getProfile()->customers);
+    $customer = $profile->getProfile()->customers->first();
+    $survey_pdf_first = $customer->surveyPdfs->first();
+    @endphp
+
+    @foreach ($survey_pdf_first->questionCharts as $question_chart)
+        <p>{{ $question_chart->question_txt }}</p>
+        <x-lime.chart type="chartjs" question_chart_id="{{ $question_chart->id }}"></x-lime.chart>
+    @endforeach
+>>>>>>> 9f56546 (.)
 @endsection
