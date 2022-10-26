34d4ef1f440c47ea5e7847b2ef6ea64bfbe6b290
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 17:00:08 +0000

    rebase

--- Resources/views/admin/dashboard/04.blade.php
+++ Resources/views/admin/dashboard/04.blade.php
@@ -23,15 +23,26 @@
     //dddx($profile->getProfile()->customers);
     $customer = $profile->getProfile()->customers->first();
     $survey_pdf_first = null;
+    $question_charts = null;
     if (!is_null($customer)) {
-        $survey_pdf_first = $customer->surveyPdfs->first();
+        $survey_pdf_first = $customer->surveyPdfs->last();
+        $question_charts = $survey_pdf_first->questionCharts;
     }
     @endphp
 
+<<<<<<< HEAD
 
     @foreach ($survey_pdf_first->questionCharts as $question_chart)
         <p>{{ $question_chart->question_txt }}</p>
         <x-lime.chart type="chartjs" id="{{ $question_chart->id }}"></x-lime.chart>
     @endforeach
 >>>>>>> 9f56546 (.)
+=======
+    @if (!is_null($question_charts))
+        @foreach ($survey_pdf_first->questionCharts as $question_chart)
+            <p>{{ $question_chart->question_txt }}</p>
+            <x-lime.chart type="chartjs" id="{{ $question_chart->id }}"></x-lime.chart>
+        @endforeach
+    @endif
+>>>>>>> cee8120 (.)
 @endsection
