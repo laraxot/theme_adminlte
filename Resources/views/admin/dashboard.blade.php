5a5035d8ded7071f3846214403c23ff75615f5c5
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 16:59:35 +0000

    rebase

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -59,17 +59,21 @@
     <livewire:time-weather-tile position="a1" />
     <livewire:time-weather-tile position="b1" />
     --}}
+    <livewire:dashboard.tile.question-chart position="" qid="15" />
+    {{--  
     <livewire:dashboard.tile.dummy position="c1" />
+    --}}
     <livewire:time-weather-tile position="d1" />
 
-    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}" position="a1:b2" />
+    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}"  position="a1:b2" />
+
     
-    {{--
+    
     @livewire('chart-tile', [
-    'chartClass' => Modules\LU\Charts\DailyUsersChart::class, 
-    'chartFilters' => ['type' => 'customer'],
+    'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class, 
+    'chartFilters' => ['question_chart_id'=>5],
     ])
-    --}}
+    
 </x-dashboard>
 
 <<<<<<< HEAD
