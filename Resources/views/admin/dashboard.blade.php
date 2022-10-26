ee11c8a63d162017cedb15e44a3eb660493dc9e7
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-26 16:59:33 +0000

    rebase

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 {{-- @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
@@ -31,10 +32,32 @@
 =======
 =======
 >>>>>>> 326067f (Revert "Auxiliary commit to revert individual files from c2e7087aeb40af8c995eee2396b2ddb4f7ee121a")
+=======
+{{--
+>>>>>>> 0e49c35 (up)
 @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
 @endsection
+--}}
+<x-dashboard>
+    {{-- replace this by any tiles --}}
+    {{--
+    <livewire:time-weather-tile position="a1" />
+    <livewire:time-weather-tile position="b1" />
+    --}}
+    <livewire:dashboard.tile.dummy position="c1" />
+    <livewire:time-weather-tile position="d1" />
+
+    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}" position="a1:b2" />
+    
+    {{--
+    @livewire('chart-tile', [
+    'chartClass' => Modules\LU\Charts\DailyUsersChart::class, 
+    'chartFilters' => ['type' => 'customer'],
+    ])
+    --}}
+</x-dashboard>
 
 <<<<<<< HEAD
 >>>>>>> 31d6e3b (first)
