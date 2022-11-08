87dfb4286b1e8fd5c6343169f7141cca602312a4
marco76tv <marco.sottana@gmail.com>
2022-11-08 18:56:33 +0100

    rebase

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -3,10 +3,14 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 905478d (rebase)
 =======
 <<<<<<< HEAD
 >>>>>>> ee11c8a (rebase)
+=======
+<<<<<<< HEAD
+>>>>>>> d1cab52 (rebase)
 {{-- @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
@@ -44,35 +48,42 @@
 {{--
 >>>>>>> 0e49c35 (up)
 @extends('adm_theme::layouts.app')
+=======
+{{-- @extends('adm_theme::layouts.app')
+>>>>>>> 9a4b06e (.)
 @section('content')
     DASHBOARD
-@endsection
---}}
+@endsection --}}
 <x-dashboard>
     {{-- replace this by any tiles --}}
-    {{--
-    <livewire:time-weather-tile position="a1" />
-    <livewire:time-weather-tile position="b1" />
-    --}}
-    <livewire:dashboard.tile.question-chart position="" qid="15" />
-    {{--  
-    <livewire:dashboard.tile.dummy position="c1" />
-    --}}
-    <livewire:time-weather-tile position="d1" />
+    {{-- <livewire:time-weather-tile position="a1" />
+    <livewire:time-weather-tile position="b1" /> --}}
 
-    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}"  position="a1:b2" />
+    {{-- <livewire:dashboard.tile.dummy position="c1" /> --}}
+    <livewire:time-weather-tile position="a1:a2" />
+    <livewire:chart-tile chartClass="{{ Modules\LU\Charts\DailyUsersChart::class }}" position="b1:b2" />
 
-    
-    
+    <livewire:chart-tile chartClass="{{ Modules\Chart\Charts\ExampleBarChart::class }}" position="a3:a4" />
+    {{-- <livewire:dashboard.tile.question-chart position="a3:a4" qid="15" /> --}}
+
+
+
     @livewire('chart-tile', [
-    'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class, 
-    'chartFilters' => ['question_chart_id'=>5],
+        'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class,
+        'chartFilters' => ['question_chart_id' => 5],
+        'position' => 'b3:b4',
     ])
-    
-</x-dashboard>
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> 31d6e3b (first)
 =======
 >>>>>>> 326067f (Revert "Auxiliary commit to revert individual files from c2e7087aeb40af8c995eee2396b2ddb4f7ee121a")
+<<<<<<< HEAD
 >>>>>>> 905478d (rebase)
+=======
+=======
+
+</x-dashboard>
+>>>>>>> 9a4b06e (.)
+>>>>>>> d1cab52 (rebase)
