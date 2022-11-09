39226822e5c3a5cd9fff63455ed599934ce023be
Malebestia <malebestia@yahoo.it>
2022-11-09 09:46:08 +0100

    rebase

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -7,12 +7,16 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> fbfdfef (rebase)
+=======
+>>>>>>> b4122f6 (rebase)
 >>>>>>> 905478d (rebase)
 =======
 <<<<<<< HEAD
 >>>>>>> ee11c8a (rebase)
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 <<<<<<< HEAD
@@ -27,6 +31,11 @@
 >>>>>>> 64fad46 (rebase)
 =======
 >>>>>>> fbfdfef (rebase)
+=======
+=======
+<<<<<<< HEAD
+>>>>>>> d1cab52 (rebase)
+>>>>>>> b4122f6 (rebase)
 {{-- @extends('adm_theme::layouts.app')
 @section('content')
     DASHBOARD
@@ -73,6 +82,7 @@
 =======
 {{-- @extends('adm_theme::layouts.app')
 >>>>>>> 9a4b06e (.)
+<<<<<<< HEAD
 @section('content')
     DASHBOARD
 @endsection --}}
@@ -105,40 +115,42 @@
 >>>>>>> 0e49c35 (up)
 >>>>>>> fbfdfef (rebase)
 @extends('adm_theme::layouts.app')
+=======
+>>>>>>> b4122f6 (rebase)
 @section('content')
     DASHBOARD
-@endsection
---}}
+@endsection --}}
 <x-dashboard>
     {{-- replace this by any tiles --}}
-    {{--
-    <livewire:time-weather-tile position="a1" />
-    <livewire:time-weather-tile position="b1" />
-    --}}
-    <livewire:dashboard.tile.question-chart position="" qid="15" />
-    {{--  
-    <livewire:dashboard.tile.dummy position="c1" />
-    --}}
-    <livewire:time-weather-tile position="d1" />
+    {{-- <livewire:time-weather-tile position="a1" />
+    <livewire:time-weather-tile position="b1" /> --}}
 
-    <livewire:chart-tile chartClass="{{Modules\LU\Charts\DailyUsersChart::class}}"  position="a1:b2" />
+    {{-- <livewire:dashboard.tile.dummy position="c1" /> --}}
+    <livewire:time-weather-tile position="a1:a2" />
+    <livewire:chart-tile chartClass="{{ Modules\LU\Charts\DailyUsersChart::class }}" position="b1:b2" />
 
-    
-    
+    <livewire:chart-tile chartClass="{{ Modules\Chart\Charts\ExampleBarChart::class }}" position="a3:a4" />
+    {{-- <livewire:dashboard.tile.question-chart position="a3:a4" qid="15" /> --}}
+
+
+
     @livewire('chart-tile', [
-    'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class, 
-    'chartFilters' => ['question_chart_id'=>5],
+        'chartClass' => Modules\Quaeris\Charts\QuestionIdChart::class,
+        'chartFilters' => ['question_chart_id' => 5],
+        'position' => 'b3:b4',
     ])
-    
-</x-dashboard>
 
 >>>>>>> 64fad46 (rebase)
+<<<<<<< HEAD
 <<<<<<< HEAD
 >>>>>>> 31d6e3b (first)
 =======
 >>>>>>> 326067f (Revert "Auxiliary commit to revert individual files from c2e7087aeb40af8c995eee2396b2ddb4f7ee121a")
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b4122f6 (rebase)
 >>>>>>> 905478d (rebase)
 =======
 =======
@@ -146,8 +158,11 @@
 </x-dashboard>
 >>>>>>> 9a4b06e (.)
 >>>>>>> d1cab52 (rebase)
+<<<<<<< HEAD
 =======
 >>>>>>> d79d738 (up)
 =======
 >>>>>>> 905478d (rebase)
 >>>>>>> 64fad46 (rebase)
+=======
+>>>>>>> b4122f6 (rebase)
