5b2c3126257b3cd41c410fc5507fdadd29864c04
marco xot <marco.sottana@gmail.com>
2022-11-09 15:28:08 +0100

    rebase

--- View/Composers/ThemeComposer.php
+++ View/Composers/ThemeComposer.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Themes\AdminLTE\View\Composers;
 
 use Illuminate\View\View;
-use Modules\Xot\View\Composers\XotBaseComposer;
+use Modules\Theme\View\Composers\XotBaseComposer;
 use Themes\AdminLTE\Services\AdminLTE;
 
 class ThemeComposer extends XotBaseComposer {
@@ -28,3 +28,76 @@ class ThemeComposer extends XotBaseComposer {
         $view->with('_theme', $this->adminlte);
     }
 }
+<<<<<<< HEAD
+=======
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 5374766d (rebase)
+}
+=======
+}
+>>>>>>> 31d6e3b (first)
+=======
+}
+>>>>>>> f405739 (up)
+<<<<<<< HEAD
+>>>>>>> f617437 (rebase)
+=======
+=======
+}
+>>>>>>> 326067f (Revert "Auxiliary commit to revert individual files from c2e7087aeb40af8c995eee2396b2ddb4f7ee121a")
+<<<<<<< HEAD
+>>>>>>> 905478d (rebase)
+=======
+=======
+}
+>>>>>>> e6202c2 (.)
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 2388cd9 (rebase)
+=======
+=======
+}
+>>>>>>> b135dba (.)
+<<<<<<< HEAD
+>>>>>>> 5fba10f (rebase)
+=======
+=======
+}
+>>>>>>> 7bb12fc (.)
+<<<<<<< HEAD
+>>>>>>> 7582ab0 (rebase)
+=======
+=======
+}
+>>>>>>> 63c2c43 (up)
+<<<<<<< HEAD
+>>>>>>> 08eedf4 (rebase)
+=======
+=======
+}
+>>>>>>> 8a5cc0b (.)
+<<<<<<< HEAD
+>>>>>>> db07ea7 (rebase)
+=======
+=======
+}
+>>>>>>> 5b4054a (.)
+>>>>>>> 5a3868d (rebase)
+=======
+}
+>>>>>>> d79d738 (up)
+=======
+>>>>>>> 2388cd9 (rebase)
+>>>>>>> 5374766d (rebase)
+>>>>>>> 524ab8cd (.)
