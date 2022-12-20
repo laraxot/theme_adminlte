67cf8ff3a74a40d77e2505e67ccfb6780545ead5
marco76tv <marco.sottana@gmail.com>
2022-12-20 14:52:47 +0100

    up

--- Services/AdminLTE.php
+++ Services/AdminLTE.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Str;
 use Modules\Theme\Models\Menu;
-use Modules\Xot\Services\PanelService;
+use Modules\Cms\Services\PanelService;
 use Modules\Xot\View\Composers\XotBaseComposer;
 use Nwidart\Modules\Facades\Module;
 use Themes\AdminLTE\Events\BuildingMenu;
