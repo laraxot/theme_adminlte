e8d2a9264878269d1867708e6df5938311badfb9
marco76tv <marco.sottana@gmail.com>
2023-01-02 12:34:33 +0100

    up

--- View/Components/BaseLayout.php
+++ View/Components/BaseLayout.php
@@ -18,7 +18,7 @@ class BaseLayout extends Component {
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     
      */
     public function render(): \Illuminate\Contracts\Support\Renderable {
         /**
