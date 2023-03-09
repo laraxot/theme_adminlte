d750439b5fbd2284d1a4c3dd5837abda773c98ba
marco76tv <marco.sottana@gmail.com>
2023-03-09 11:39:26 +0100

    up

--- Config/button.php
+++ Config/button.php
@@ -1,65 +1,64 @@
-<?php return array (
-  'action' => 
+<?php
+
+return array (
+  'action' =>
   array (
-    'v1' => 
+    'v1' =>
     array (
       'class' => 'btn btn-primary',
     ),
   ),
-  'link' => 
+  'link' =>
   array (
-    'v1' => 
+    'v1' =>
     array (
       'class' => 'btn btn-primary',
     ),
-    'tab' => 
+    'tab' =>
     array (
       'class' => 'nav-link',
     ),
   ),
-  'v1' => 
+  'v1' =>
   array (
     'class' => 'btn btn-primary',
   ),
-  'panel' => 
+  'panel' =>
   array (
-    'create' => 
+    'create' =>
     array (
-      'v1' => 
+      'v1' =>
       array (
         'class' => 'btn btn-sm btn-primary',
       ),
     ),
-    'v1' => 
+    'v1' =>
     array (
-      'show' => 
+      'show' =>
       array (
         'icon' => '<i class="far fa-eye"></i>',
         'class' => 'btn btn-primary',
       ),
-      'detach' => 
+      'detach' =>
       array (
         'icon' => '<i class="fas fa-unlink"></i>',
         'class' => 'btn btn-primary',
       ),
-      'delete' => 
+      'delete' =>
       array (
         'icon' => '<i class="far fa-trash-alt"></i>',
         'class' => 'btn btn-danger',
       ),
-<<<<<<< HEAD
-=======
-      'edit' => 
+      'edit' =>
       array (
         'icon' => '<i class="fas fa-pencil-alt"></i>',
         'class' => 'btn btn-primary',
       ),
-      'create' => 
+      'create' =>
       array (
         'icon' => '<i class="fas fa-plus"></i>',
         'class' => 'btn btn-primary',
       ),
->>>>>>> 316e7e21 (..)
     ),
   ),
-);
+);
