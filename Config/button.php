d09ce2ee8eeb0bf436a7eeab23803da87f80d3a6
Malebestia <vair81@gmail.com>
2023-03-13 15:46:50 +0100

    up

--- Config/button.php
+++ Config/button.php
@@ -1,86 +1,51 @@
 <?php
 
-return array(
-  'action' =>
-  array(
-    'v1' =>
-    array(
-      'class' => 'btn btn-primary',
-    ),
-  ),
-  'link' =>
-  array(
-    'v1' =>
-    array(
-      'class' => 'btn btn-primary',
-    ),
-    'tab' =>
-    array(
-      'class' => 'nav-link',
-    ),
-  ),
-  'v1' =>
-  array(
-    'class' => 'btn btn-primary',
-  ),
-  'panel' =>
-  array(
-    'create' =>
-    array(
-      'v1' =>
-      array(
-        'class' => 'btn btn-sm btn-primary',
-      ),
-    ),
-    'v1' =>
-    array(
-      'show' =>
-      array(
-        'icon' => '<i class="far fa-eye"></i>',
+declare(strict_types=1);
+
+return [
+    'action' => [
+        'v1' => [
+            'class' => 'btn btn-primary',
+        ],
+    ],
+    'link' => [
+        'v1' => [
+            'class' => 'btn btn-primary',
+        ],
+        'tab' => [
+            'class' => 'nav-link',
+        ],
+    ],
+    'v1' => [
         'class' => 'btn btn-primary',
-      ),
-      'detach' =>
-      array(
-        'icon' => '<i class="fas fa-unlink"></i>',
-        'class' => 'btn btn-primary',
-      ),
-      'delete' =>
-      array(
-        'icon' => '<i class="far fa-trash-alt"></i>',
-        'class' => 'btn btn-danger',
-      ),
-<<<<<<< HEAD
-<<<<<<< HEAD
-      'edit' =>
-      array(
-        'icon' => '<i class="fas fa-pencil-alt"></i>',
-        'class' => 'btn btn-primary',
-      ),
-      'create' =>
-      array(
-        'icon' => '<i class="fas fa-plus"></i>',
-        'class' => 'btn btn-primary',
-      ),
-=======
-<<<<<<< HEAD
-=======
->>>>>>> e69d303b (..)
-=======
-      'edit' => 
-      array (
-        'icon' => '<i class="fas fa-pencil-alt"></i>',
-        'class' => 'btn btn-primary',
-      ),
-      'create' => 
-      array (
-        'icon' => '<i class="fas fa-plus"></i>',
-        'class' => 'btn btn-primary',
-      ),
->>>>>>> 316e7e21 (..)
-<<<<<<< HEAD
->>>>>>> e69d303b (..)
-=======
->>>>>>> e69d303b (..)
-    ),
-  ),
-);
+    ],
+    'panel' => [
+        'create' => [
+            'v1' => [
+                'class' => 'btn btn-sm btn-primary',
+            ],
+        ],
+        'v1' => [
+            'show' => [
+                'icon' => '<i class="far fa-eye"></i>',
+                'class' => 'btn btn-primary',
+            ],
+            'detach' => [
+                'icon' => '<i class="fas fa-unlink"></i>',
+                'class' => 'btn btn-primary',
+            ],
+            'delete' => [
+                'icon' => '<i class="far fa-trash-alt"></i>',
+                'class' => 'btn btn-danger',
+            ],
+            'edit' => [
+                'icon' => '<i class="fas fa-pencil-alt"></i>',
+                'class' => 'btn btn-primary',
+            ],
+            'create' => [
+                'icon' => '<i class="fas fa-plus"></i>',
+                'class' => 'btn btn-primary',
+            ],
+        ],
+    ],
+];
