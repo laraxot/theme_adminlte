a19c8744e84b4e68c6de9df2d4f646730775734c
marco xot <marco.sottana@gmail.com>
2023-04-20 14:38:12 +0200

    up

--- Config/button.php
+++ Config/button.php
@@ -1,52 +1,70 @@
-<?php
-
-
-declare(strict_types=1);
-
-return [
-    'action' => [
-        'v1' => [
-            'class' => 'btn btn-primary',
-        ],
-    ],
-    'link' => [
-        'v1' => [
-            'class' => 'btn btn-primary',
-        ],
-        'tab' => [
-            'class' => 'nav-link',
-        ],
-    ],
-    'v1' => [
+<?php return array (
+  'action' => 
+  array (
+    'v1' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+  ),
+  'link' => 
+  array (
+    'v1' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+    'tab' => 
+    array (
+      'class' => 'nav-link',
+    ),
+  ),
+  'v1' => 
+  array (
+    'class' => 'btn btn-primary',
+    'button' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+    'submit' => 
+    array (
+      'class' => 'btn btn-primary',
+    ),
+  ),
+  'panel' => 
+  array (
+    'create' => 
+    array (
+      'v1' => 
+      array (
+        'class' => 'btn btn-sm btn-primary',
+      ),
+    ),
+    'v1' => 
+    array (
+      'show' => 
+      array (
+        'icon' => '<i class="far fa-eye"></i>',
         'class' => 'btn btn-primary',
-    ],
-    'panel' => [
-        'create' => [
-            'v1' => [
-                'class' => 'btn btn-sm btn-primary',
-            ],
-        ],
-        'v1' => [
-            'show' => [
-                'icon' => '<i class="far fa-eye"></i>',
-                'class' => 'btn btn-primary',
-            ],
-            'detach' => [
-                'icon' => '<i class="fas fa-unlink"></i>',
-                'class' => 'btn btn-primary',
-            ],
-            'delete' => [
-                'icon' => '<i class="far fa-trash-alt"></i>',
-                'class' => 'btn btn-danger',
-            ],
-            'edit' => [
-                'icon' => '<i class="fas fa-pencil-alt"></i>',
-                'class' => 'btn btn-primary',
-            ],
-            'create' => [
-                'icon' => '<i class="fas fa-plus"></i>',
-                'class' => 'btn btn-primary',
-            ],
-        ],
-    ],
-];
+      ),
+      'detach' => 
+      array (
+        'icon' => '<i class="fas fa-unlink"></i>',
+        'class' => 'btn btn-primary',
+      ),
+      'delete' => 
+      array (
+        'icon' => '<i class="far fa-trash-alt"></i>',
+        'class' => 'btn btn-danger',
+      ),
+      'edit' => 
+      array (
+        'icon' => '<i class="fas fa-pencil-alt"></i>',
+        'class' => 'btn btn-primary',
+      ),
+      'create' => 
+      array (
+        'icon' => '<i class="fas fa-plus"></i>',
+        'class' => 'btn btn-primary',
+      ),
+    ),
+  ),
+);
